6c6df38675df85885f4c1fe7da653683ed98fd25
Eliza Clayton <eliza.aclayton@gmail.com>
2024-12-19 18:19:10 +0000

    CreateTable: TrailUser

---
+++ SQLQuery_CreateTable_TrailUser.sql
@@ -0,0 +1,8 @@
+CREATE TABLE CW2.TrailUser(
+    UserID INT NOT NULL IDENTITY,
+    emailAddress VARCHAR (320) NOT NULL,
+    roleType VARCHAR (5) NOT NULL
+
+    CONSTRAINT PK_User PRIMARY KEY (UserID),
+    CONSTRAINT UQ_email UNIQUE (emailAddress)
+)
