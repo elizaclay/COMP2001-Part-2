16d47c5e813ac70834d6cbbda58914043720ecfa
Eliza Clayton <eliza.aclayton@gmail.com>
2024-12-23 16:58:41 +0000

    Populate PathPoint

--- SQLQuery_Populate_Tables.sql
+++ SQLQuery_Populate_Tables.sql
@@ -1,4 +1,4 @@
-INSERT INTO CW2.Trail (TrailName, TrailSummary, TrailDescription, Distance_km, ElevationGain_m, TimeEstimate_min, DifficultyType, RouteType, LocationType, OwnerID)
+INSERT INTO CW1.PathPoint (TrailID, Longitude,Latitude,PathSequence,Details)
 
-VALUES 
-('Plymouth Circular','A scenic circular trail in Plymouth','Explore the beautiful Plymouth through this lovely circular trail.',5.0,147,83,1,3,1,1)
+VALUES
+((SELECT TrailID FROM CW1.Trail WHERE trailName = 'Plymouth Circular'),50.40886,-4.07859,1, 'Start/End')
