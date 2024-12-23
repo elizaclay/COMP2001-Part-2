07460d548c60933830cdc47bd96c10fa2039ea0a
Eliza Clayton <eliza.aclayton@gmail.com>
2024-12-23 16:33:52 +0000

    Populate Trail

--- SQLQuery_Populate_Tables.sql
+++ SQLQuery_Populate_Tables.sql
@@ -1,6 +1,4 @@
-INSERT INTO CW2.TrailUser (EmailAddress, RoleType)
-VALUES
-('grace@plymouth.ac.uk','Admin'), 
-('tim@plymouth.ac.uk', 'User'), 
-('ada@plymouth.ac.uk', 'User')
+INSERT INTO CW2.Trail (TrailName, TrailSummary, TrailDescription, Distance_km, ElevationGain_m, TimeEstimate_min, DifficultyType, RouteType, LocationType, OwnerID)
 
+VALUES 
+('Plymouth Circular','A scenic circular trail in Plymouth','Explore the beautiful Plymouth through this lovely circular trail.',5.0,147,83,1,3,1,1)
